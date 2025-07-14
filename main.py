cef9c24cd7b92682e5c4fc572bf4d72f1fb53b97
M-chan <new.jasjan.programmer@gmail.com>
2025-07-14 19:43:41 +0500

    some code scale

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from typing import Dict, List
 from fastapi.middleware.cors import CORSMiddleware
-
-import database  # noqa
 
 app = FastAPI()
 
-# Allow Next.js frontend to connect
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # frontend URL
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+rooms: Dict[str, List[WebSocket]] = {}
 
-@app.websocket("/vc")
-async def websocket_endpoint(websocket: WebSocket):
+@app.websocket("/vc/{room_id}")
+async def websocket_endpoint(websocket: WebSocket, room_id: str):
     await websocket.accept()
 
+    if room_id not in rooms:
+        rooms[room_id] = []
+    rooms[room_id].append(websocket)
+
     try:
-        await websocket.send_text("Welcome to websocket")
-
         while True:
-            data = await websocket.receive_text()
-            print("Received:", data)
-            await websocket.send_text(f"Echo: {data}")
-    except WebSocketDisconnect:
-        print("Client disconnected")
+            message = await websocket.receive_json()
 
+            for client in rooms[room_id]:
+                if client != websocket:
+                    await client.send_json(message)
+
+    except WebSocketDisconnect:
+        rooms[room_id].remove(websocket)
+        print(f"Client disconnected from room {room_id}")
+        if not rooms[room_id]:
+            del rooms[room_id]
